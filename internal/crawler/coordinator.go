a2fa03f5d9a166c733239bf2081d667b382e74b0
Yuhao Chen <gentoobreaking@gmail.com>
2026-09-06 16:06:34 +0800

    T090: Fix crawler coordinator getSourceTrustScore to use DefaultSourceTrustScores

--- internal/crawler/coordinator.go
+++ internal/crawler/coordinator.go
@@ -362,15 +362,15 @@ func (c *CrawlCoordinator) normalizeServers(
 func getSourceTrustScore(source string) float64 {
 	switch source {
 	case "github":
-		return models.SourceTrustScores{}.GitHub
+		return models.DefaultSourceTrustScores.GitHub
 	case "registry":
-		return models.SourceTrustScores{}.Registry
+		return models.DefaultSourceTrustScores.Registry
 	case "mcpserversorg":
-		return models.SourceTrustScores{}.Mcpserversorg
+		return models.DefaultSourceTrustScores.Mcpserversorg
 	case "mcpmarket":
-		return models.SourceTrustScores{}.Mcpmarket
+		return models.DefaultSourceTrustScores.Mcpmarket
 	case "githubrepo":
-		return models.SourceTrustScores{}.GithubRepo
+		return models.DefaultSourceTrustScores.GithubRepo
 	default:
 		return 0.5
 	}
